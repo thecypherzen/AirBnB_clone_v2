d05bbb96c924d64f7b7800ee4d0d56051d25a514
William Inyam <sw.inyam@outlook.com>
2024-06-11 23:27:56 +0000

    fix(hbnb2:models): fix numberous issues

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-
+import os
 
 storage = FileStorage()
+storage_t = os.getenv("HBNB_TYPE_STORAGE")
 storage.reload()

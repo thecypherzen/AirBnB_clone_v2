f1d6986cf4040267332edd1493e48ab1869ea62f
William Inyam <sw.inyam@outlook.com>
2024-06-11 19:14:13 +0000

    docs(hbnb2:flask): enforce pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,16 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
+
+# run app
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
             port=5000)

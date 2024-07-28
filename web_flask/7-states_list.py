7efc6783ae0205f5c2233af5f7c67a0cafcddb66
William Inyam <sw.inyam@outlook.com>
2024-07-28 09:53:41 +0000

    revert(hbnb2:flask:7): undo template trims"

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,8 +7,8 @@ from models.state import State
 
 
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/states_list", strict_slashes=False)

fd7ce59a0292ca9b86b747b9806993c4735178da
William Inyam <sw.inyam@outlook.com>
2024-08-02 13:18:20 +0000

    fix(hbnb2:fabric:5): fix manifest errors

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,41 +1,45 @@
 # sets up a new machine for deploy backup
-exec {'update packages':
+exec { 'update packages':
   command => 'apt update',
-  path    => '/usr/bin'
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
 }
 
--> package {'install nginx':
+package { 'install nginx':
   ensure   => installed,
-  name     => 'nginx',
-  provider => 'apt',
+  name     => nginx,
+  provider => apt,
 }
 
--> exec {'open ufw ports':
+exec { 'open ufw ports':
   command => 'ufw allow 80/tcp && ufw allow 22/tcp',
-  path    => '/usr/sbin',
+  path    => ['/usr/sbin', '/usr/bin']
 }
 
--> exec {'create directories':
+exec { 'create directories':
   command => 'mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
-  path    => '/usr/bin',
+  path    => ['/usr/bin', '/usr/sbin']
 }
 
--> exec {'update test content':
+exec { 'update test content':
   command => 'echo "I am ready" | tee /data/web_static/releases/test/index.html > /dev/null',
-  path    => '/usr/bin',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
--> exec { 'create symlink':
+file {'/data/web_static/current':
+  ensure  => absent,
+}
+
+exec { 'create symlink':
   command => 'ln -s /data/web_static/releases/test/ /data/web_static/current',
-  path    => 'usr/bin',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
--> exec { 'change ownership':
+exec { 'change ownership':
   command => 'chown -R ubuntu:ubuntu /data/',
-  path    => '/usr/bin',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
-$server_config = @("CONFIG"/L)
+$nginx_config = @(CONFIG/$)
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -45,23 +49,23 @@ server {
     server_name _;
 
     add_header X-Served-By \$hostname always;
-
     location / {
-       try_files \$uri \$uri/ =404;
+          try_files \$uri \$uri/ =404;
     }
 
     location /hbnb_static {
-        alias /data/web_static/current;
+         alias /data/web_static/current;
     }
 }
-CONFIG
+ | - CONFIG
 
--> file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => $server_config,
+
+file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => $nginx_config,
 }
 
--> exec { 'restart nginx':
+exec { 'restart nginx':
   command => 'service nginx restart',
-  path    => '/usr/sbin',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
